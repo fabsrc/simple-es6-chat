3648c80243ed21390e0a0a5ec4b9c90d7fce486a
Fabian Schneider <fschneider.mmt-m2014@fh-salzburg.ac.at>
2015-01-30 12:05:55 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('serve', function() {
+
+});
+
+gulp.task('build:js', function() {
+
+});
+
+gulp.task('build:css', function() {
+
+});
+
+gulp.task('test', function() {
+
+});
+
+gulp.task('doc', function() {
+
+});
